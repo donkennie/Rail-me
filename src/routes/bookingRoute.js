713cfc4a82104f1fa5ebeeab8a9bf07ec3ce83ca
donkennie <ajeigbekehinde160@gmail.com>
2022-10-18 15:26:03 +0100

    fix error from the booking route

--- src/routes/bookingRoute.js
+++ src/routes/bookingRoute.js
@@ -7,5 +7,16 @@ const booking = require('../controllers/bookingController');
 router.route('/createBooking')
       .post(booking.CreateBooking);
 
+router.route('/updateBooking')
+      .patch(booking.UpdateBooking);
+
+router.route('/getAllBooking')
+      .get(booking.GetAllBooking);
+
+router.route('/getBookingById/:id')
+      .get(booking.GetBooking);
+
+router.route('/deleteBooking/:id')
+      .delete(booking.DeleteBooking);
 
 module.exports = router;
